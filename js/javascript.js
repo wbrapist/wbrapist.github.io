dc0b3f8440561afe56a41d05697efafc83f498e7
wbrapist <wbrapist@gmail.com>
2017-08-01 04:59:51 +0300

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,20 +1,23 @@
+'use strict';
+
 openNavMenu();
 
 function openNavMenu() {
   var navMenuButton = document.getElementsByClassName('nav-button')[0];
   var content = document.getElementsByClassName('content')[0];
   var mobileMenu = document.getElementsByClassName('mobile-version')[0];
+  var mobileMenuHeight = getComputedStyle(mobileMenu).height;
 
   navMenuButton.onclick = function() {
-    var isContentHide = content.className.indexOf('hide');
+    var isContentHidden = content.className.indexOf('hide');
 
-    if (isContentHide != -1) {
+    if (isContentHidden != -1) {
       content.className = 'content' + ' show';
       mobileMenu.style.height = 'auto';
     }
-    else if (isContentHide == -1) {
+    else if (isContentHidden == -1) {
       content.className = 'content' + ' hide';
-      mobileMenu.style.height = '60px'; // width of nav.mobile-version
+      mobileMenu.style.height = mobileMenuHeight;
     }
   };
 }
