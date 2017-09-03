7a23e53ba4e6021752ef439e1c28d8df91441c10
wbrapist <wbrapist@gmail.com>
2017-09-04 00:53:07 +0300

    added content

--- js/main.js
+++ js/main.js
@@ -1,21 +1,23 @@
-var content = document.getElementsByClassName('content')[0];
-var compContent = getComputedStyle(content);
-var compContentHeight = parseFloat(compContent.height);
+window.onload = function() {
+  var content = document.getElementsByClassName('content')[0];
+  var compContent = getComputedStyle(content);
+  var compContentHeight = parseFloat(compContent.height);
 
-var nav = document.getElementsByTagName('nav')[0];
-var compNav = getComputedStyle(nav);
-var compNavHeight = parseFloat(compNav.height);
+  var nav = document.getElementsByTagName('nav')[0];
+  var compNav = getComputedStyle(nav);
+  var compNavHeight = parseFloat(compNav.height);
 
-var maxHeight = '600px';
+  var maxHeight = '600px';
 
-nav.onmouseover = function() {
-  if ( compContentHeight >= (600 - compNavHeight) ) {
-    this.style.height = maxHeight;
-    content.style.height = maxHeight;
+  nav.onmouseover = function() {
+    if ( compContentHeight >= (600 - compNavHeight) ) {
+      this.style.height = maxHeight;
+      content.style.height = maxHeight;
+    }
+    else
+      this.style.height = compContentHeight + compNavHeight + 'px';
   }
-  else
-    this.style.height = compContentHeight + compNavHeight + 'px';
-}
-nav.onmouseout = function() {
-  this.style.height = compNavHeight + 'px';
-}
+  nav.onmouseout = function() {
+    this.style.height = compNavHeight + 'px';
+  }
+};
